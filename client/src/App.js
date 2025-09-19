4be2ccbbcf3421379806cbbbfb6b4106fd914bfc
Yash <nirmalyash721@gmail.com>
2025-09-19 13:50:53 +0530

    Modified UI Files

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,20 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Register from "./components/Register";
-import Login from "./components/Login";
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 function App() {
   return (
-    <Router>
+    <div className="App">
+      <nav>
+        <Link to="/register">Register</Link> |{" "}
+        <Link to="/login">Login</Link>
+      </nav>
       <Routes>
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
       </Routes>
-    </Router>
+    </div>
   );
 }
 
